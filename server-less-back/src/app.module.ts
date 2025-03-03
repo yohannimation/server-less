124ce583aad81c230fc2613bef20befe1d04200f
Yohann <yrenauld@myges.fr>
2025-03-03 12:31:09 +0100

    delete(): back (unused)

--- server-less-back/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FileModule } from './file/file.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { AnalysisModule } from './analysis/analysis.module';
-
-@Module({
-  imports: [PrismaModule, FileModule, AnalysisModule],
-})
-export class AppModule {}
